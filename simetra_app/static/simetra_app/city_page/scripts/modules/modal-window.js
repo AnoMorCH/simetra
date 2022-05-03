8a8c2434a7760ea91f65baa1fff9790aec05bf74
klekkwedge <antonov.andrey.10101@gmail.com>
2022-05-03 12:05:38 +0300

    fix: disable swipe when opening a modal window

--- simetra_app/static/simetra_app/city_page/scripts/modules/modal-window.js
+++ simetra_app/static/simetra_app/city_page/scripts/modules/modal-window.js
@@ -2,28 +2,30 @@
 
 function modalWindow() {
   const overlay = document.querySelector(".overlay"),
-    modal = document.querySelector(".modal"),
-    modalContent = document.querySelector(".modal__content"),
     modalClose = document.querySelector(".modal__close");
 
   modalClose.addEventListener("click", () => {
     overlay.style.display = "none";
+    document.body.classList.remove("_lock");
   });
 
   overlay.addEventListener("click", (event) => {
     if (event.target === overlay) {
       overlay.style.display = "none";
+      document.body.classList.remove("_lock");
     }
   });
 
   document.addEventListener("keydown", (event) => {
     if (event.code === "Escape" && overlay.style.display === "block") {
       overlay.style.display = "none";
+      document.body.classList.remove("_lock");
     }
   });
 
   if (window.innerWidth < 700) {
     overlay.style.display = "block";
+    document.body.classList.add("_lock");
   }
 }
 
